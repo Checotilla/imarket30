71f3d23730a8bd1c8f0de5c826f62d59b2895c6c
Valentin Kosarev <astitstudio@gmail.com>
2013-03-08 23:05:22 +0400

    Идексные страницы
    index.php для основного проекта
    index.php для ymp

---
+++ www/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>iMarket30</title>
+
+        <style type="text/css">
+            body{
+                background-color: #ffcc99;
+            }
+            td{
+                padding:30px;
+            }
+            
+            .header{
+                background-color: #ffcccc;
+            }
+            .main{
+                background-color: #ffffff;
+                height: 80%;
+                vertical-align: top;
+            }
+            .footer{
+                background-color: #ffcccc;
+            }
+
+        </style>
+
+    </head>
+    <body>
+
+        <table border="0" width ="70%" align="center" height="800px">
+<!-- HEADER -->
+            <tr class="header">
+                <td>
+                    <h1>iMarket30 - Regional internet market</h1>
+
+                </td>
+            </tr>
+
+<!-- MAIN -->
+            <tr class="main">
+                <td>
+                    <h3>
+                        Projects:
+                        <ol>
+                            <li>Yandex-market Parser <a href="http://ymp.imarket30.loc">http://ymp.imarket30.loc</a></li>
+                        </ol>
+
+
+
+                </td>
+            </tr>
+<!-- FOOTER -->
+            <tr class="footer">
+                <td>
+                    <a href="http://imarket30.loc">iMarket 30</a>
+                    |
+                    <a href="http://ymp.imarket30.loc">Yandex-market Parser</a>
+                </td>
+            </tr>
+        </table>
+
+    </h3>
+</body>
+</html>
---
+++ ymp/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>YMP: iMarket30</title>
+
+        <style type="text/css">
+            body{
+                background-color: #ccccff;
+            }
+            td{
+                padding:30px;
+            }
+            
+            .header{
+                background-color: #9999ff;
+            }
+            .main{
+                background-color: #ffffff;
+                height: 80%;
+                vertical-align: top;
+            }
+            .footer{
+                background-color: #9999ff;
+            }
+
+        </style>
+
+    </head>
+    <body>
+
+        <table border="0" width ="70%" align="center" height="800px">
+<!-- HEADER -->
+            <tr class="header">
+                <td>
+                    <h1>Yandex-market parser</h1>
+
+                </td>
+            </tr>
+
+<!-- MAIN -->
+            <tr class="main">
+                <td>
+                    <h3>
+                        Projects:
+                        <ol>
+                            <li>Yandex-market Parser <a href="http://ymp.imarket30.loc">http://ymp.imarket30.loc</a></li>
+                            <li></li>
+                        </ol>
+
+
+
+                </td>
+            </tr>
+<!-- FOOTER -->
+            <tr class="footer">
+                <td>
+                    <a href="http://imarket30.loc">iMarket 30</a>
+                    |
+                    <a href="http://ymp.imarket30.loc">Yandex-market Parser</a>
+                </td>
+            </tr>
+        </table>
+
+    </h3>
+</body>
+</html>
